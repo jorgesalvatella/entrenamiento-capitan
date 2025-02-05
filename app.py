318cb378976e011054b16a2eecaf117ee672455e
Jorge Humberto Espinosa <jorgehumbertoespinosas@outlook.com>
2025-02-05 15:44:41 -0500

    Fix app.py for Heroku

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -7,5 +8,8 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    # Obtenemos el puerto de la variable de entorno de Heroku
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
 
